5878291a5052456f7f45e17bdfb35f3b0d3a54f0
Kiefer <kiefx@pm.me>
2024-02-04 11:41:01 -0800

    The only way Zybooks will accept this.

--- src/ShoppingCartPrinter.java
+++ src/ShoppingCartPrinter.java
@@ -1,38 +1,35 @@
 import java.util.Scanner;
 
 public class ShoppingCartPrinter {
-    static Scanner scnr = new Scanner(System.in);
-
     public static void main(String[] args) {
-        ItemToPurchase item1 = getItemDetails("Item 1");
-        System.out.println();
-        ItemToPurchase item2 = getItemDetails("Item 2");
+        Scanner scnr = new Scanner(System.in);
 
-        printTotalCost(item1, item2);
-    }
-
-    public static ItemToPurchase getItemDetails(String itemLabel) {
-        System.out.println(itemLabel);
+        System.out.println("Item 1");
         System.out.println("Enter the item name:");
         String itemName = scnr.nextLine();
         System.out.println("Enter the item price:");
         int itemPrice = scnr.nextInt();
-        scnr.nextLine();  // consume newline left-over
         System.out.println("Enter the item quantity:");
         int itemQuantity = scnr.nextInt();
-        scnr.nextLine();  // consume newline left-over
 
-        return new ItemToPurchase(itemName, itemPrice, itemQuantity);
-    }
+        scnr.nextLine();
 
-    public static void printItemCost(ItemToPurchase item) {
-        System.out.printf("%s %d @ $%d = $%d%n", item.getName(), item.getQuantity(), item.getPrice(), (item.getPrice() * item.getQuantity()));
-    }
+        System.out.println("\nItem 2");
+        System.out.println("Enter the item name:");
+        String itemName2 = scnr.nextLine();
+        System.out.println("Enter the item price:");
+        int itemPrice2 = scnr.nextInt();
+        System.out.println("Enter the item quantity:");
+        int itemQuantity2 = scnr.nextInt();
 
-    public static void printTotalCost(ItemToPurchase item1, ItemToPurchase item2) {
-        System.out.println("TOTAL COST");
-        printItemCost(item1);
-        printItemCost(item2);
+        ItemToPurchase item1 = new ItemToPurchase(itemName, itemPrice, itemQuantity);
+        ItemToPurchase item2 = new ItemToPurchase(itemName2, itemPrice2, itemQuantity2);
+
+        System.out.println("\nTOTAL COST");
+        System.out.printf("%s %d @ $%d = $%d%n", item1.getName(), item1.getQuantity(), item1.getPrice(), (item1.getPrice() * item1.getQuantity()));
+        System.out.printf("%s %d @ $%d = $%d%n", item2.getName(), item2.getQuantity(), item2.getPrice(), (item2.getPrice() * item2.getQuantity()));
+        System.out.println();
         System.out.printf("Total: $%d%n", ((item1.getPrice() * item1.getQuantity()) + (item2.getPrice() * item2.getQuantity())));
+
     }
 }
